91a8a329934796913f50be2a61e70560ec62a5b5
Igor Andrusyak <andrusyak.igor@gmail.com>
2015-12-01 16:49:38 +0200

    Update script.js

--- GoIT/JavaScript/Lesson_1-2/task_1/js/script.js
+++ GoIT/JavaScript/Lesson_1-2/task_1/js/script.js
@@ -4,12 +4,16 @@ for (var i = 0; i < 5; i++) {
 	arr.unshift(prompt('Введите имя', ''));
 }
 
-var enter = prompt('Укажите свое имя', '');
+var enterName = prompt('Укажите свое имя', '');
 
-if (enter == arr[0] || enter == arr[1] || enter == arr[2] || enter == arr[3] || enter == arr[4]) {
-		alert('Вы успешно вошли ' + enter); 
-	} else {
-		alert('Ошибка');
-	}
+for (var i = 0; i < arr.length; i++) {
+    if (enterName == arr[i]) {
+        alert(enterName + ' Вы успешно вошли');
+        break;
+    }
+}
+if(enterName != arr[i]){
+    alert('Ошибка');
+}
 
 
