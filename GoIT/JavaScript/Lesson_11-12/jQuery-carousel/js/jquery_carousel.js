4be444fab5ced95d6197506f17ecc9d20fd9c50c
Igor Andrusyak <andrusyak.igor@gmail.com>
2016-01-28 00:17:00 +0200

    added lesson_11-12

---
+++ GoIT/JavaScript/Lesson_11-12/jQuery-carousel/js/jquery_carousel.js
@@ -0,0 +1,47 @@
+(function ($) {
+
+	$.fn.carousel = function(options) {
+
+		var defaults = {
+			widthHider: 600,
+			heightHider: 100,
+			showElem: 3
+		};
+
+		var settings = $.extend(defaults, options);
+		
+		var $setWidthHider = defaults.widthHider + 'px';
+		var $setHeightHider = defaults.heightHider + 'px';
+		var $setShowElem = defaults.showElem;
+
+		$('.carousel-hider').innerWidth($setWidthHider).innerHeight($setHeightHider);
+
+		var $left = $('.carousel-arrow-left');
+		var $right = $('.carousel-arrow-right');
+		var $elementsList = $('.carousel-list');
+		var $widthImg = $('.carousel-list img').width();
+		var $currentLeftValue = 0;
+		var $elementsCount = $elementsList.find('li').length;
+		var $minimumOffset = - (($elementsCount - $setShowElem) * $widthImg);
+		var $maximumOffset = 0;
+
+		function leftMove() {
+			if ($currentLeftValue != $maximumOffset) {
+				$currentLeftValue += $widthImg;
+				$elementsList.animate({ left : $currentLeftValue + "px"}, 500);
+			}   
+		};
+
+		function rightMove() {
+			if ($currentLeftValue != $minimumOffset) {
+				$currentLeftValue -= $widthImg;
+				$elementsList.animate({ left : $currentLeftValue + "px"}, 500);
+			}   
+		};
+
+		$left.on('click', leftMove);
+		$right.on('click', rightMove);
+
+}
+
+})(jQuery);
---
+++ GoIT/JavaScript/Lesson_11-12/jQuery-carousel/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  $('.carousel-button').carousel({
+    widthHider: 600,
+    heightHider: 150,
+    showElem: 3
+  });
+  
+});
