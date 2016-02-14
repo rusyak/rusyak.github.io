6ba9edaed08f1dc3e92d6542d167632dac3a474e
Igor Andrusyak <andrusyak.igor@gmail.com>
2016-02-14 22:53:48 +0200

    added JS_17-18

---
+++ GoIT/JavaScript/JS_17-18/js/dist/script.main.js
@@ -0,0 +1,3 @@
+var name = "My name is Igor.";
+console.log('name:', name);;var dreamWork = "I want to be a front-end developer."
+console.log('dreamWork:', dreamWork);
---
+++ GoIT/JavaScript/JS_17-18/js/src/script1.js
@@ -0,0 +1,2 @@
+var name = "My name is Igor.";
+console.log('name:', name);
