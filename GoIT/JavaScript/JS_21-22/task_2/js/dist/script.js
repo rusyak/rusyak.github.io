27f89e7fb9074d280d355c9182fce250782da69e
Igor Andrusyak <andrusyak.igor@gmail.com>
2016-03-12 19:40:28 +0200

    added JS_21-22\task_2

---
+++ GoIT/JavaScript/JS_21-22/task_2/js/dist/script.js
@@ -0,0 +1,175 @@
+$(function(){
+'use strict';
+
+var test = [{
+  title: 'How to write an IF statement in JavaScript?',
+  points: 1,
+  answers: [{
+    answer: 'if (i == 5)',
+    right: true
+  }, {
+    answer: 'if i = 5 then',
+    right: false
+  }, {
+    answer: 'if i == 5 then',
+    right: false
+  }]
+}, {
+  title: 'How do you round the number 7.25, to the nearest integer?',
+  points: 1,
+  answers: [{
+    answer: 'round(7.25)',
+    right: false
+  }, {
+    answer: 'rnd(7.25)',
+    right: false
+  }, {
+    answer: 'Math.round(7.25)',
+    right: true
+  }]
+}, {
+  title: 'How do you declare a JavaScript variable?',
+  points: 1,
+  answers: [{
+    answer: 'v carName',
+    right: false
+  }, {
+    answer: 'var carName',
+    right: true
+  }, {
+    answer: 'variable carName',
+    right: false
+  }]
+}, {
+  title: 'What will the following code return: Boolean(10 > 9)',
+  points: 1,
+  answers: [{
+    answer: 'NaN',
+    right: false
+  }, {
+    answer: 'true',
+    right: true
+  }, {
+    answer: 'false',
+    right: false
+  }]
+}];
+
+// sending object to local storage
+var localTest = JSON.stringify(test);
+localStorage.setItem("test", localTest);
+
+// and retrieving it
+var recievedTest = localStorage.getItem("test");
+var readyTest = JSON.parse(recievedTest);
+
+// preparing template
+var $html = $('#template').html();
+var content = tmpl($html, {
+  data: readyTest
+});
+
+// inserting template into DOM
+$('#formId').prepend(content);
+
+var $inputs = $('input:checkbox');
+$inputs.on('click', function () {
+  $(this).parent().siblings().children().each(function () {
+    if ($(this).attr('disabled')) {
+      $(this).attr('disabled', false);
+    } else {
+      $(this).attr('disabled', true);
+    }
+  });
+});
+
+var checkResults = function checkResults(e) {
+  e.preventDefault();
+  var rightAnswers = [];
+  var getRightAnswers = function getRightAnswers() {
+    for (var i = 0; i < readyTest.length; i++) {
+      var testAnswers = readyTest[i].answers;
+      for (var j = 0; j < testAnswers.length; j++) {
+        var currentAnswer = readyTest[i].answers[j].right;
+        rightAnswers.push(currentAnswer);
+      }
+    }
+  };
+
+  var givenAnswers = [];
+  var getGivenAnswers = function getGivenAnswers() {
+    $inputs.each(function () {
+      if ($(this).prop('checked')) {
+        givenAnswers.push(true);
+      } else {
+        givenAnswers.push(false);
+      }
+    });
+  };
+
+  var answered = 0;
+  var check = function check() {
+    for (var i = 0; i < rightAnswers.length; i++) {
+      if (rightAnswers[i] === true) {
+        if (rightAnswers[i] === givenAnswers[i]) {
+          answered++;
+        }
+      }
+    }
+  };
+
+  var questionsQuantity = 0;
+  var sumQuestions = function sumQuestions() {
+    for (var i = 0; i < readyTest.length; i++) {
+      questionsQuantity++;
+    }
+  };
+
+  var passed = 0;
+  var testOK = false;
+  var testPassed = function testPassed() {
+    passed = answered / questionsQuantity;
+    if (passed > 0.5) {
+      testOK = true;
+    }
+  };
+
+  getRightAnswers();
+  console.log('rightAnswers = ', rightAnswers);
+
+  getGivenAnswers();
+  console.log('givenAnswers = ', givenAnswers);
+
+  check();
+  console.log('answered = ', answered);
+
+  sumQuestions();
+
+  testPassed();
+  console.log('passed = ', passed);
+
+  console.log('testOK = ', testOK);
+
+  // building modal with test results
+  var $modal = void 0;
+  var $body = $('body');
+  if (testOK) {
+    $modal = '<div class="mymodal"><div class="mymodal-inner"><h1 class="text-center">You passed the test!</h1><h1 class="text-center">Right is ' + answered + ', from ' + questionsQuantity + '</h1><p class="text-center"><img src="img/icon-good.png"></p><a class="center-block btn btn-primary" id="exit">Exit</a></div></div>';
+  } else {
+    $modal = '<div class="mymodal"><div class="mymodal-inner"><h1 class="text-center">You didn\'t pass the test!</h1><h1 class="text-center">Right is ' + answered + ', from ' + questionsQuantity + '</h1><p class="text-center"><img src="img/icon-bad.png"></p><a class="center-block btn btn-primary" id="exit">Exit</a></div></div>';
+  }
+
+  $body.append($modal);
+  var $exit = $('#exit');
+  var reset = function reset() {
+    $inputs.prop('checked', false).prop('disabled', false);
+    $('.mymodal').remove();
+    return false;
+  };
+
+  $exit.on('click', reset);
+};
+
+$('#check-results').on('click', checkResults);
+
+});
---
+++ GoIT/JavaScript/JS_21-22/task_2/js/src/script.js
@@ -0,0 +1,186 @@
+let test = [
+{
+  title: 'How to write an IF statement in JavaScript?',
+  points: 1,
+  answers: [{
+    answer: 'if (i == 5)',
+    right: true
+  },
+  {
+    answer: 'if i = 5 then',
+    right: false
+  },
+  {
+    answer: 'if i == 5 then',
+    right: false
+  }]
+},
+{
+  title: 'How do you round the number 7.25, to the nearest integer?',
+  points: 1,
+  answers: [{
+    answer: 'round(7.25)',
+    right: false
+  },
+  {
+    answer: 'rnd(7.25)',
+    right: false
+  },
+  {
+    answer: 'Math.round(7.25)',
+    right: true
+  }]
+},
+{
+  title: 'How do you declare a JavaScript variable?',
+  points: 1,
+  answers: [{
+    answer: 'v carName',
+    right: false
+  },
+  {
+    answer: 'var carName',
+    right: true
+  },
+  {
+    answer: 'variable carName',
+    right: false
+  }]
+},
+{
+  title: 'What will the following code return: Boolean(10 > 9)',
+  points: 1,
+  answers: [{
+    answer: 'NaN',
+    right: false
+  },
+  {
+    answer: 'true',
+    right: true
+  },
+  {
+    answer: 'false',
+    right: false
+  }]
+}
+];
+
+// sending object to local storage
+let localTest = JSON.stringify( test );
+localStorage.setItem( "test", localTest );
+
+// and retrieving it
+let recievedTest = localStorage.getItem( "test" );
+let readyTest = JSON.parse( recievedTest );
+
+// preparing template
+let $html = $( '#template' ).html();
+let content = tmpl($html, {
+  data: readyTest
+});
+
+// inserting template into DOM
+$( '#formId' ).prepend( content );
+
+let $inputs = $('input:checkbox');
+$inputs.on( 'click', function() {
+  $(this).parent().siblings().children().each(function(){
+    if ( $(this).attr('disabled') ) {
+      $(this).attr('disabled', false);
+    } else {
+      $(this).attr('disabled', true);
+    }
+  });
+});
+
+let checkResults = function(e) {
+  e.preventDefault();
+  let rightAnswers = [];
+  let getRightAnswers = function() {
+    for ( let i = 0; i < readyTest.length; i++ ) {
+      let testAnswers = readyTest[i].answers;
+      for (let j = 0; j < testAnswers.length; j++) {
+        let currentAnswer = readyTest[i].answers[j].right;
+        rightAnswers.push(currentAnswer);
+      }
+    }
+  };
+
+  let givenAnswers = [];
+  let getGivenAnswers = function() {
+    $inputs.each(function () {
+      if ( $(this).prop('checked') ) {
+        givenAnswers.push(true);
+      } else {
+        givenAnswers.push(false);
+      }
+    });
+  };
+
+  let answered = 0;
+  let check = function () {
+    for (let i = 0; i < rightAnswers.length; i++) {
+      if ( rightAnswers[i] === true ) {
+        if ( rightAnswers[i] === givenAnswers[i] ) {
+          answered++;
+        }
+      }
+    }
+  };
+
+  let questionsQuantity = 0;
+  let sumQuestions = function () {
+    for (let i = 0; i < readyTest.length; i++) {
+      questionsQuantity++;
+    }
+  };
+
+  let passed = 0;
+  let testOK= false;
+  let testPassed = function () {
+    passed = answered /questionsQuantity;
+    if ( passed > 0.5 ) {
+      testOK = true;
+    }
+  };
+
+  getRightAnswers();
+  console.log('rightAnswers = ', rightAnswers);
+
+  getGivenAnswers();
+  console.log('givenAnswers = ', givenAnswers);
+
+  check();
+  console.log('answered = ', answered);
+
+  sumQuestions();
+
+  testPassed();
+  console.log('passed = ', passed);
+
+  console.log('testOK = ', testOK);
+
+
+// building modal with test results
+  let $modal;
+  let $body = $( 'body' );
+  if ( testOK ){
+    $modal = ('<div class="mymodal"><div class="mymodal-inner"><h1 class="text-center">You passed the test!</h1><h1 class="text-center">Right is '+
+     answered +', from '+ questionsQuantity +'</h1><p class="text-center"><img src="img/icon-good.png"></p><a class="center-block btn btn-primary" id="exit">Exit</a></div></div>');
+  } else {
+    $modal = ('<div class="mymodal"><div class="mymodal-inner"><h1 class="text-center">You didn\'t pass the test!</h1><h1 class="text-center">Right is '+
+     answered +', from '+ questionsQuantity +'</h1><p class="text-center"><img src="img/icon-bad.png"></p><a class="center-block btn btn-primary" id="exit">Exit</a></div></div>');
+  }
+
+  $body.append($modal);
+  let $exit = $( '#exit' );
+  let reset = function() {
+    $inputs.prop( 'checked', false ).prop( 'disabled', false );
+    $( '.mymodal' ).remove();
+    return false;
+  };
+
+  $exit.on( 'click', reset );
+};
+
+$( '#check-results' ).on( 'click', checkResults );
