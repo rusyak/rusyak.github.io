68bf9aa2a5836fefa93ad16276b9c5638a4c40d8
Igor Andrusyak <andrusyak.igor@gmail.com>
2016-05-07 12:53:20 +0300

    fixed ajax request and some css styles

--- GoIT/JavaScript/JS_25_exam/src/js/script.js
+++ GoIT/JavaScript/JS_25_exam/src/js/script.js
@@ -29,67 +29,41 @@ $(function(){
     target: '+=1'
   });
 
+  
+
+  var search = '';
+
+  function getImgByRequest(search){
 
   $.ajax({
-    url: 'http://api.pixplorer.co.uk/image?word=natural&amount=7&size=m',
+    type: 'GET',
+    dataType: 'json',
+    url: 'http://api.pixplorer.co.uk/image?word=' + search + '&amount=7&size=thumbnail',
     success: function(data){
-      var model = new Model(data);
-      var view = new View(model);
-      var controller = new Controller(model, view);
+      var html = $('#template').html();
+      var content = tmpl(html, {data: data.images});
+
+      $('.grid').remove();
+      $('.ideas__title').append(content);
+      $('.grid').isotope({
+        itemSelector: '.tile--ideas',
+        layoutMode: 'masonry',
+        masonry: {
+                gutter: 5
+            }
+      });
     }
   });
-
-  function Model(obj){
-    var self = this;
-    self.init = function(data){
-      var array = [];
-      var obj = {
-        url: 'none',
-        word: 'none'
-      };
-      for(var index in data.images)
-      {
-        obj.url = data.images[index].imageurl;
-        obj.word = data.images[index].word;
-        array.push(data.images[index]);
-      }
-      self.data = array;
-    };
-    self.init(obj);
-  }
-//v
-function View(model){
-  var self = this;
-  self.renderList = function () {
-
-
-    var wrapper = tmpl($('#template').html(), model);
-    $('.ideas').append(wrapper);
-
-    $('#gallery').imagesLoaded(function (){
-     $('.grid').masonry({
-      itemSelector: '.grid--item',
-      columnWidth: '.grid--item',
-      gutter: 10
-    });
-   });
-  };
-  self.renderList();
 }
-//c
-function Controller(model, view){
-  var self = this;
-  $('#search').on('click', function(event){
-    event.preventDefault();
-    var text = $('#edit').val();
-    $('.grid').remove();
-    $.ajax({
-      url: 'http://api.pixplorer.co.uk/image?word='+text+'&amount=7&size=m',
-      success: function(data){
-        model.init(data);
-        view.renderList();
-      }
+ 
+    $('.form__input').on('click', function(e) {
+        e.preventDefault();
+        var query = $('.form__edit');
+        getImgByRequest(encodeURIComponent(query.val()));
+        query.val('');
     });
-  });
-}
+
+
+    getImgByRequest();
+
 });
